33965fc4773ea1fceacc7637a6a61fcdb988d831
marat-rkh <marat.r.khabibullin@gmail.com>
2024-12-04 14:56:59 +0200

    Day 2: part 2 brute force complete

--- day2/main.go
+++ day2/main.go
@@ -33,14 +33,75 @@ func Solve1() {
 }
 
 func isSafe(levels []int) bool {
-	firstDiff := 0
-	for i := 0; i < len(levels)-1; i++ {
-		diff := levels[i] - levels[i+1]
-		absDiff := int(math.Abs(float64(diff)))
-		if firstDiff == 0 {
-			firstDiff = diff
+	diffs := calcDiffs(levels)
+	posCount, negCount, _ := countSigns(diffs)
+	return (posCount == len(diffs) || negCount == len(diffs)) && isInRange(diffs)
+}
+
+func Solve2BruteForce() {
+	log.SetOutput(io.Discard) // io.Discard or os.Stdout
+
+	data, err := os.ReadFile("day2/in1.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	reports := strings.Split(string(data), "\n")
+	safeCount := 0
+	for _, report := range reports {
+		entries := strings.Split(report, " ")
+		levels := []int{}
+		for _, entry := range entries {
+			level, _ := strconv.Atoi(entry)
+			levels = append(levels, level)
 		}
-		if !(0 < absDiff && absDiff < 4 && (firstDiff > 0 && diff > 0 || firstDiff < 0 && diff < 0)) {
+		log.Println("")
+		log.Printf("Checking: %v\n", levels)
+		if isSafe(levels) {
+			log.Println("Safe")
+			safeCount++
+			continue
+		}
+		for i := 0; i < len(levels); i++ {
+			levelsWithSkip := make([]int, len(levels)-1)
+			copy(levelsWithSkip, levels[:i])
+			copy(levelsWithSkip[i:], levels[i+1:])
+			if isSafe(levelsWithSkip) {
+				log.Printf("Safe with skip at index %d\n", i)
+				safeCount++
+				break
+			}
+		}
+	}
+	fmt.Println(safeCount)
+}
+
+func calcDiffs(levels []int) []int {
+	diffs := make([]int, 0, len(levels)-1)
+	for i := 0; i < len(levels)-1; i++ {
+		diffs = append(diffs, levels[i]-levels[i+1])
+	}
+	return diffs
+}
+
+func countSigns(diffs []int) (int, int, int) {
+	posCount, negCount, zeroCount := 0, 0, 0
+	for _, diff := range diffs {
+		if diff > 0 {
+			posCount++
+		} else if diff < 0 {
+			negCount++
+		} else {
+			zeroCount++
+		}
+	}
+	return posCount, negCount, zeroCount
+}
+
+func isInRange(diffs []int) bool {
+	for _, diff := range diffs {
+		absDiff := int(math.Abs(float64(diff)))
+		if !(0 < absDiff && absDiff < 4) {
 			return false
 		}
 	}
@@ -69,16 +130,7 @@ func Solve2() {
 
 		diffs := calcDiffs(levels)
 		log.Printf("Diffs: %v\n", diffs)
-		posCount, negCount, zeroCount := 0, 0, 0
-		for _, diff := range diffs {
-			if diff > 0 {
-				posCount++
-			} else if diff < 0 {
-				negCount++
-			} else {
-				zeroCount++
-			}
-		}
+		posCount, negCount, _ := countSigns(diffs)
 		if posCount == len(diffs) || negCount == len(diffs) {
 			log.Println("All pos or all neg")
 			if isInRange(diffs) {
@@ -107,24 +159,6 @@ func Solve2() {
 	fmt.Println(safeCount)
 }
 
-func calcDiffs(levels []int) []int {
-	diffs := make([]int, 0, len(levels)-1)
-	for i := 0; i < len(levels)-1; i++ {
-		diffs = append(diffs, levels[i]-levels[i+1])
-	}
-	return diffs
-}
-
-func isInRange(diffs []int) bool {
-	for _, diff := range diffs {
-		absDiff := int(math.Abs(float64(diff)))
-		if !(0 < absDiff && absDiff < 4) {
-			return false
-		}
-	}
-	return true
-}
-
 func indexOf[T any](slice []T, predicate func(T) bool) int {
 	for i, v := range slice {
 		if predicate(v) {
@@ -144,46 +178,4 @@ func isSafeWithSkip(levels []int, skipIndex int) bool {
 	copy(levelsWithSkip[skipIndex:], levels[skipIndex+1:])
 	diffs1 := calcDiffs(levelsWithSkip)
 	return isInRange(diffs1)
-}
-
-func Solve3() {
-	log.SetOutput(io.Discard) // io.Discard or os.Stdout
-
-	data, err := os.ReadFile("day2/in1.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	reports := strings.Split(string(data), "\n")
-	safeCount := 0
-	for _, report := range reports {
-		entries := strings.Split(report, " ")
-		levels := []int{}
-		for _, entry := range entries {
-			level, _ := strconv.Atoi(entry)
-			levels = append(levels, level)
-		}
-		if isSafe(levels) {
-			log.Printf("Safe: %v\n", levels)
-			safeCount++
-			continue
-		}
-		log.Printf("Trying skips for: %v\n", levels)
-		for i := 0; i < len(levels)-1; i++ {
-			log.Printf("Levels at iteration %d: %v \n", i, levels)
-			pref := levels[:i]
-			suff := levels[i+1:]
-			log.Printf("Pref & suff: %v %v\n", pref, suff)
-			levelsWithSkip := make([]int, len(levels)-1)
-			copy(levelsWithSkip, pref)
-			copy(levelsWithSkip[i:], suff)
-			log.Printf("Levels with skip: %v\n", levelsWithSkip)
-			if isSafe(levelsWithSkip) {
-				log.Printf("Safe: %v\n", levels)
-				safeCount++
-				break
-			}
-		}
-	}
-	fmt.Println(safeCount)
 }
