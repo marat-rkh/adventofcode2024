8258f27fb7c1b7b4dc595b4dadea752496d8edb0
marat-rkh <marat.r.khabibullin@gmail.com>
2024-12-07 21:38:31 +0200

    Day 6: advanced marks

--- day6/main.go
+++ day6/main.go
@@ -16,12 +16,15 @@ const (
 	Left  = '<'
 
 	Obstruction = '#'
-	Marked      = 'X'
+	Empty       = '.'
+	MarkVert    = '|'
+	MarkHoriz   = '-'
+	MarkCross   = '+'
 )
 
 func Solve1() {
 	log.SetOutput(os.Stdout)
-	data, err := os.ReadFile("day6/in1.txt")
+	data, err := os.ReadFile("day6/in0.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -39,39 +42,43 @@ func Solve1() {
 			}
 		}
 	}
+	direction0 := field[i0][j0]
+
 	i, j := i0, j0
+	direction := direction0
 	for 0 <= i && i < len(field) && 0 <= j && j < len(field[0]) {
-		switch field[i][j] {
+		switch direction {
 		case Up:
 			i = moveUp(field, i, j)
 			if i >= 0 {
-				field[i][j] = Right
+				direction = Right
 			}
 		case Right:
 			j = moveRight(field, i, j)
 			if j < len(field[i]) {
-				field[i][j] = Down
+				direction = Down
 			}
 		case Down:
 			i = moveDown(field, i, j)
 			if i < len(field) {
-				field[i][j] = Left
+				direction = Left
 			}
 		case Left:
 			j = moveLeft(field, i, j)
 			if j >= 0 {
-				field[i][j] = Up
+				direction = Up
 			}
 		default:
 			panic("invalid direction")
 		}
 	}
+	field[i0][j0] = direction0
 	logField(field)
 
 	markedCount := 0
 	for i := 0; i < len(field); i++ {
 		for j := 0; j < len(field[i]); j++ {
-			if field[i][j] == Marked {
+			if field[i][j] != Empty && field[i][j] != Obstruction {
 				markedCount++
 			}
 		}
@@ -83,42 +90,62 @@ func moveUp(field [][]rune, i, j int) int {
 	for ; i >= 0; i-- {
 		if field[i][j] == Obstruction {
 			i++
+			field[i][j] = MarkCross
 			break
 		}
-		field[i][j] = Marked
+		if field[i][j] == MarkHoriz {
+			field[i][j] = MarkCross
+		} else {
+			field[i][j] = MarkVert
+		}
 	}
 	return i
 }
 
 func moveRight(field [][]rune, i, j int) int {
+	j0 := j
 	for ; j < len(field[i]); j++ {
 		if field[i][j] == Obstruction {
 			j--
 			break
 		}
-		field[i][j] = Marked
+		if j == j0 || field[i][j] == MarkVert {
+			field[i][j] = MarkCross
+		} else {
+			field[i][j] = MarkHoriz
+		}
 	}
 	return j
 }
 
 func moveDown(field [][]rune, i, j int) int {
+	i0 := i
 	for ; i < len(field); i++ {
 		if field[i][j] == Obstruction {
 			i--
 			break
 		}
-		field[i][j] = Marked
+		if i == i0 || field[i][j] == MarkHoriz {
+			field[i][j] = MarkCross
+		} else {
+			field[i][j] = MarkVert
+		}
 	}
 	return i
 }
 
 func moveLeft(field [][]rune, i, j int) int {
+	j0 := j
 	for ; j >= 0; j-- {
 		if field[i][j] == Obstruction {
 			j++
 			break
 		}
-		field[i][j] = Marked
+		if j == j0 || field[i][j] == MarkVert {
+			field[i][j] = MarkCross
+		} else {
+			field[i][j] = MarkHoriz
+		}
 	}
 	return j
 }
