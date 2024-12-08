6d6e2dc11483a14ca40689d8917b2e18322ee347
marat-rkh <marat.r.khabibullin@gmail.com>
2024-12-09 00:16:49 +0200

    Day 6: part 2 attempt 1

--- day6/main.go
+++ day6/main.go
@@ -17,14 +17,15 @@ const (
 	Down  = 'v'
 	Left  = '<'
 
-	Obstruction = '#'
-	Empty       = '.'
-	MarkVert    = '|'
-	MarkHoriz   = '-'
-	MarkCross   = '+'
+	Obstruction      = '#'
+	Empty            = '.'
+	MarkVert         = '|'
+	MarkHoriz        = '-'
+	MarkCross        = '+'
+	AddedObstruction = 'O'
 )
 
-func Solve1() {
+func Solve() {
 	log.SetOutput(os.Stdout)
 	data, err := os.ReadFile("day6/in1.txt")
 	if err != nil {
@@ -46,30 +47,30 @@ func Solve1() {
 	}
 	direction0 := field[i0][j0]
 
-	// TODO traverse the field 4 times in each direction and calc closest obstructions for each cell
-	// TODO using closest obstructions and trace, fill a set of looping obstructions in the loop below
+	closestObstructions := calcClosestObstructions(field)
 	i, j := i0, j0
 	direction := direction0
-	trace := make(map[[2]int]mapset.Set[rune])
+	trace := make(map[[2]int]*mapset.Set[rune])
+	addedObstructions := mapset.New[[2]int]()
 	for 0 <= i && i < len(field) && 0 <= j && j < len(field[0]) {
 		switch direction {
 		case Up:
-			i = moveUp(field, i, j, trace)
+			i = moveUp(field, i, j, closestObstructions, trace, &addedObstructions)
 			if i >= 0 {
 				direction = Right
 			}
 		case Right:
-			j = moveRight(field, i, j, trace)
+			j = moveRight(field, i, j, closestObstructions, trace, &addedObstructions)
 			if j < len(field[i]) {
 				direction = Down
 			}
 		case Down:
-			i = moveDown(field, i, j, trace)
+			i = moveDown(field, i, j, closestObstructions, trace, &addedObstructions)
 			if i < len(field) {
 				direction = Left
 			}
 		case Left:
-			j = moveLeft(field, i, j, trace)
+			j = moveLeft(field, i, j, closestObstructions, trace, &addedObstructions)
 			if j >= 0 {
 				direction = Up
 			}
@@ -77,7 +78,7 @@ func Solve1() {
 			panic("invalid direction")
 		}
 	}
-	logField(field, trace)
+	logField(field, trace, &addedObstructions)
 
 	visitedCount := 0
 	for i := 0; i < len(field); i++ {
@@ -87,70 +88,165 @@ func Solve1() {
 			}
 		}
 	}
-	fmt.Println(visitedCount)
+	fmt.Printf("Visited: %d\n", visitedCount)
+	// Incorrect answers: 531 (too low)
+	fmt.Printf("Added obstructions: %d\n", addedObstructions.Size())
 }
 
-func moveUp(field [][]rune, i, j int, trace map[[2]int]mapset.Set[rune]) int {
+type obstructions struct {
+	above      [2]int
+	toTheRight [2]int
+	toTheLeft  [2]int
+	below      [2]int
+}
+
+func calcClosestObstructions(field [][]rune) [][]*obstructions {
+	closestObstructions := make([][]*obstructions, len(field))
+	for i := 0; i < len(field); i++ {
+		closestObstructions[i] = make([]*obstructions, len(field[i]))
+		for j := 0; j < len(field[i]); j++ {
+			closestObstructions[i][j] = &obstructions{}
+		}
+	}
+	for i := 0; i < len(field); i++ {
+		iObs, jObs := -1, -1
+		for j := 0; j < len(field[i]); j++ {
+			if field[i][j] == Obstruction {
+				iObs, jObs = i, j
+			}
+			closestObstructions[i][j].toTheLeft = [2]int{iObs, jObs}
+		}
+	}
+	for i := 0; i < len(field); i++ {
+		iObs, jObs := -1, -1
+		for j := len(field[i]) - 1; j >= 0; j-- {
+			if field[i][j] == Obstruction {
+				iObs, jObs = i, j
+			}
+			closestObstructions[i][j].toTheRight = [2]int{iObs, jObs}
+		}
+	}
+	for j := 0; j < len(field[0]); j++ {
+		iObs, jObs := -1, -1
+		for i := 0; i < len(field); i++ {
+			if field[i][j] == Obstruction {
+				iObs, jObs = i, j
+			}
+			closestObstructions[i][j].above = [2]int{iObs, jObs}
+		}
+	}
+	for j := 0; j < len(field[0]); j++ {
+		iObs, jObs := -1, -1
+		for i := len(field) - 1; i >= 0; i-- {
+			if field[i][j] == Obstruction {
+				iObs, jObs = i, j
+			}
+			closestObstructions[i][j].below = [2]int{iObs, jObs}
+		}
+	}
+	return closestObstructions
+}
+
+func moveUp(field [][]rune, i, j int, closestObs [][]*obstructions, trace map[[2]int]*mapset.Set[rune], addedObs *mapset.Set[[2]int]) int {
 	for ; i >= 0; i-- {
 		if field[i][j] == Obstruction {
 			i++
 			break
+		}
+		if i-1 >= 0 && field[i-1][j] != Obstruction {
+			obsToTheRight := closestObs[i][j].toTheRight
+			iObs, jObs := obsToTheRight[0], obsToTheRight[1]
+			if iObs != -1 && jObs != -1 {
+				if marks, ok := trace[[2]int{iObs, jObs - 1}]; ok && marks.Has(Right) {
+					addedObs.Put([2]int{i - 1, j})
+				}
+			}
 		}
 		getOrInit(trace, i, j).Put(Up)
 	}
 	return i
 }
 
-func moveRight(field [][]rune, i, j int, trace map[[2]int]mapset.Set[rune]) int {
+func moveRight(field [][]rune, i, j int, closestObs [][]*obstructions, trace map[[2]int]*mapset.Set[rune], addedObs *mapset.Set[[2]int]) int {
 	for ; j < len(field[i]); j++ {
 		if field[i][j] == Obstruction {
 			j--
 			break
+		}
+		if j+1 < len(field[i]) && field[i][j+1] != Obstruction {
+			obsBelow := closestObs[i][j].below
+			iObs, jObs := obsBelow[0], obsBelow[1]
+			if iObs != -1 && jObs != -1 {
+				if marks, ok := trace[[2]int{iObs - 1, jObs}]; ok && marks.Has(Down) {
+					addedObs.Put([2]int{i, j + 1})
+				}
+			}
 		}
 		getOrInit(trace, i, j).Put(Right)
 	}
 	return j
 }
 
-func moveDown(field [][]rune, i, j int, trace map[[2]int]mapset.Set[rune]) int {
+func moveDown(field [][]rune, i, j int, closestObs [][]*obstructions, trace map[[2]int]*mapset.Set[rune], addedObs *mapset.Set[[2]int]) int {
 	for ; i < len(field); i++ {
 		if field[i][j] == Obstruction {
 			i--
 			break
+		}
+		if i+1 < len(field) && field[i+1][j] != Obstruction {
+			obsToTheLeft := closestObs[i][j].toTheLeft
+			iObs, jObs := obsToTheLeft[0], obsToTheLeft[1]
+			if iObs != -1 && jObs != -1 {
+				if marks, ok := trace[[2]int{iObs, jObs + 1}]; ok && marks.Has(Left) {
+					addedObs.Put([2]int{i + 1, j})
+				}
+			}
 		}
 		getOrInit(trace, i, j).Put(Down)
 	}
 	return i
 }
 
-func moveLeft(field [][]rune, i, j int, trace map[[2]int]mapset.Set[rune]) int {
+func moveLeft(field [][]rune, i, j int, closestObs [][]*obstructions, trace map[[2]int]*mapset.Set[rune], addedObs *mapset.Set[[2]int]) int {
 	for ; j >= 0; j-- {
 		if field[i][j] == Obstruction {
 			j++
 			break
+		}
+		if j-1 >= 0 && field[i][j-1] != Obstruction {
+			obsAbove := closestObs[i][j].above
+			iObs, jObs := obsAbove[0], obsAbove[1]
+			if iObs != -1 && jObs != -1 {
+				if marks, ok := trace[[2]int{iObs + 1, jObs}]; ok && marks.Has(Up) {
+					addedObs.Put([2]int{i, j - 1})
+				}
+			}
 		}
 		getOrInit(trace, i, j).Put(Left)
 	}
 	return j
 }
 
-func getOrInit(trace map[[2]int]mapset.Set[rune], i, j int) mapset.Set[rune] {
+func getOrInit(trace map[[2]int]*mapset.Set[rune], i, j int) *mapset.Set[rune] {
 	if _, ok := trace[[2]int{i, j}]; !ok {
-		trace[[2]int{i, j}] = mapset.New[rune]()
+		marks := mapset.New[rune]()
+		trace[[2]int{i, j}] = &marks
 	}
 	return trace[[2]int{i, j}]
 }
 
-func logField(field [][]rune, trace map[[2]int]mapset.Set[rune]) {
+func logField(field [][]rune, trace map[[2]int]*mapset.Set[rune], addedObs *mapset.Set[[2]int]) {
 	var fieldWithTrace strings.Builder
 	fieldWithTrace.WriteRune('\n')
 	for i := 0; i < len(field); i++ {
 		for j := 0; j < len(field[i]); j++ {
-			if field[i][j] == Empty {
-				if trace, ok := trace[[2]int{i, j}]; ok {
-					if (trace.Has(Up) || trace.Has(Down)) && (trace.Has(Left) || trace.Has(Right)) {
+			if addedObs.Has([2]int{i, j}) {
+				fieldWithTrace.WriteRune(AddedObstruction)
+			} else if field[i][j] == Empty {
+				if marks, ok := trace[[2]int{i, j}]; ok {
+					if (marks.Has(Up) || marks.Has(Down)) && (marks.Has(Left) || marks.Has(Right)) {
 						fieldWithTrace.WriteRune(MarkCross)
-					} else if trace.Has(Up) || trace.Has(Down) {
+					} else if marks.Has(Up) || marks.Has(Down) {
 						fieldWithTrace.WriteRune(MarkVert)
 					} else {
 						// trace.Has(Left) || trace.Has(Right)
