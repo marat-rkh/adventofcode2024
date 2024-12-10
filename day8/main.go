a11020bc6e668fcc7ed73b4a94db10a5678a253d
marat-rkh <marat.r.khabibullin@gmail.com>
2024-12-10 21:11:20 +0200

    Day 8: part 2 complete

--- day8/main.go
+++ day8/main.go
@@ -74,3 +74,83 @@ func DoSolve1(lines []string) int {
 	}
 	return antinodeSet.Size()
 }
+
+func Solve2() {
+	lines := util.ReadInput("day8/in1.txt")
+	res := DoSolve2(lines)
+	fmt.Println(res)
+}
+
+func DoSolve2(lines []string) int {
+	antennasByType := make(map[rune]*mapset.Set[[2]int])
+	for i, line := range lines {
+		for j, antenna := range line {
+			if antenna != '.' {
+				if _, ok := antennasByType[antenna]; !ok {
+					antennaSet := mapset.New[[2]int]()
+					antennasByType[antenna] = &antennaSet
+				}
+				antennasByType[antenna].Put([2]int{i, j})
+			}
+		}
+	}
+	antinodeSet := mapset.New[[2]int]()
+	for _, antennaSet := range antennasByType {
+		antennas := make([][2]int, 0, antennaSet.Size())
+		antennaSet.Each(func(antenna [2]int) {
+			antennas = append(antennas, antenna)
+		})
+		for i := 0; i < len(antennas); i++ {
+			for j := i + 1; j < len(antennas); j++ {
+				antinodeSet.Put(antennas[i])
+				antinodeSet.Put(antennas[j])
+				xDiff := antennas[j][0] - antennas[i][0]
+				absXDiff := xDiff
+				if absXDiff < 0 {
+					absXDiff = -absXDiff
+				}
+				yDiff := antennas[j][1] - antennas[i][1]
+				absYDiff := yDiff
+				if absYDiff < 0 {
+					absYDiff = -absYDiff
+				}
+				for k := 1; ; k++ {
+					antinode1 := [2]int{-1, -1}
+					antinode2 := [2]int{-1, -1}
+					absXDiff := absXDiff * k
+					absYDiff := absYDiff * k
+					if xDiff >= 0 {
+						if yDiff >= 0 {
+							antinode1 = [2]int{antennas[i][0] - absXDiff, antennas[i][1] - absYDiff}
+							antinode2 = [2]int{antennas[j][0] + absXDiff, antennas[j][1] + absYDiff}
+						} else {
+							antinode1 = [2]int{antennas[i][0] - absXDiff, antennas[i][1] + absYDiff}
+							antinode2 = [2]int{antennas[j][0] + absXDiff, antennas[j][1] - absYDiff}
+						}
+					} else {
+						if yDiff >= 0 {
+							antinode1 = [2]int{antennas[i][0] + absXDiff, antennas[i][1] - absYDiff}
+							antinode2 = [2]int{antennas[j][0] - absXDiff, antennas[j][1] + absYDiff}
+						} else {
+							antinode1 = [2]int{antennas[i][0] + absXDiff, antennas[i][1] + absYDiff}
+							antinode2 = [2]int{antennas[j][0] - absXDiff, antennas[j][1] - absYDiff}
+						}
+					}
+					isAdded := false
+					if antinode1[0] >= 0 && antinode1[0] < len(lines) && antinode1[1] >= 0 && antinode1[1] < len(lines[0]) {
+						antinodeSet.Put(antinode1)
+						isAdded = true
+					}
+					if antinode2[0] >= 0 && antinode2[0] < len(lines) && antinode2[1] >= 0 && antinode2[1] < len(lines[0]) {
+						antinodeSet.Put(antinode2)
+						isAdded = true
+					}
+					if !isAdded {
+						break
+					}
+				}
+			}
+		}
+	}
+	return antinodeSet.Size()
+}
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/marat-rkh/adventofcode2024/day8"
 
 func main() {
-	day8.Solve1()
+	day8.Solve2()
 }
