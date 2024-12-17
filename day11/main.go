beffbec36e19a87b6307765ef0931fefe11e021e
marat-rkh <marat.r.khabibullin@gmail.com>
2024-12-17 22:27:58 +0200

    Day 11: part 2 attempt 2

--- day11/main.go
+++ day11/main.go
@@ -20,16 +20,25 @@ func Solve2(file string) int {
 func solve(file string, iterations int) int {
 	curNums := ReadInput(file)
 	nextNums := []int64{}
+	splitCache := map[int64][2]int64{}
+	mul2024Cache := map[int64]int64{}
 	for i := 0; i < iterations; i++ {
 		for _, num := range curNums {
 			if num == 0 {
 				nextNums = append(nextNums, 1)
+			} else if split, ok := splitCache[num]; ok {
+				nextNums = append(nextNums, split[0], split[1])
+			} else if mul2024, ok := mul2024Cache[num]; ok {
+				nextNums = append(nextNums, mul2024)
 			} else if digitsCount := countDigits(num); digitsCount%2 == 0 {
 				divisor := int64(math.Pow10(digitsCount / 2))
 				num1, num2 := num/divisor, num%divisor
 				nextNums = append(nextNums, num1, num2)
+				splitCache[num] = [2]int64{num1, num2}
 			} else {
-				nextNums = append(nextNums, num*2024)
+				mul2024 := num * 2024
+				nextNums = append(nextNums, mul2024)
+				mul2024Cache[num] = mul2024
 			}
 		}
 		curNums = nextNums
