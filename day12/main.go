0ee175ce6bc66b675f4e9b270f930ab2e8399a6c
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2024-12-21 13:49:29 +0200

    Day 12: refactor

--- day12/main.go
+++ day12/main.go
@@ -7,49 +7,28 @@ import (
 
 func Solve1(file string) int {
 	field := util.ReadInput(file)
-	measurements := [][3]int{}
-	visited := make(map[[2]int]bool)
-	for i := 0; i < len(field); i++ {
-		for j := 0; j < len(field[i]); j++ {
-			if visited[[2]int{i, j}] {
-				continue
-			}
-			queue := [][2]int{{i, j}}
-			curArea := 0
-			curPerimeter := 0
-			for len(queue) > 0 {
-				cur := queue[0]
-				queue = queue[1:]
-				if visited[cur] {
-					continue
-				}
-				visited[cur] = true
-				curArea++
-				moves := [][2]int{
-					{cur[0], cur[1] - 1}, // left
-					{cur[0], cur[1] + 1}, // right
-					{cur[0] - 1, cur[1]}, // up
-					{cur[0] + 1, cur[1]}, // down
-				}
-				for _, move := range moves {
-					if !isWithinBounds(move, field) || field[move[0]][move[1]] != field[cur[0]][cur[1]] {
-						curPerimeter++
-					} else {
-						queue = append(queue, move)
-					}
+	plots := calcPlots(field)
+	areas := make(map[int]int)
+	perimeters := make(map[int]int)
+	for i := range plots {
+		for j := range plots[i] {
+			areas[plots[i][j]]++
+			moves := [][2]int{{i, j - 1}, {i, j + 1}, {i - 1, j}, {i + 1, j}}
+			for _, move := range moves {
+				if !isWithinBounds(move, field) || field[move[0]][move[1]] != field[i][j] {
+					perimeters[plots[i][j]]++
 				}
 			}
-			measurements = append(measurements, [3]int{int(field[i][j]), curArea, curPerimeter})
 		}
 	}
+
 	res := 0
-	for _, measurement := range measurements {
-		res += measurement[1] * measurement[2]
+	for plotId, area := range areas {
+		res += area * perimeters[plotId]
 	}
 	return res
 }
 
-// TODO: re-use in Solve1
 func calcPlots(field []string) [][]int {
 	plots := make([][]int, len(field))
 	for i := range plots {
@@ -71,17 +50,11 @@ func calcPlots(field []string) [][]int {
 				}
 				visited[cur] = true
 				plots[cur[0]][cur[1]] = plotId
-				moves := [][2]int{
-					{cur[0], cur[1] - 1}, // left
-					{cur[0], cur[1] + 1}, // right
-					{cur[0] - 1, cur[1]}, // up
-					{cur[0] + 1, cur[1]}, // down
-				}
+				moves := [][2]int{{cur[0], cur[1] - 1}, {cur[0], cur[1] + 1}, {cur[0] - 1, cur[1]}, {cur[0] + 1, cur[1]}}
 				for _, move := range moves {
-					if !isWithinBounds(move, field) || field[move[0]][move[1]] != field[cur[0]][cur[1]] {
-						continue
+					if isWithinBounds(move, field) && field[move[0]][move[1]] == field[cur[0]][cur[1]] {
+						queue = append(queue, move)
 					}
-					queue = append(queue, move)
 				}
 			}
 			plotId++
