e24d47d7edbcfeebb1b5d3db1a05d08b0ce84116
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2024-12-26 13:39:16 +0200

    Day 13: part 2 complete

--- zig/src/day13/solution.zig
+++ zig/src/day13/solution.zig
@@ -18,7 +18,15 @@ const testing = std.testing;
 // Db = 94 * 5400 - 34 * 8400 = 222000
 // a = Da / D = 80
 // b = Db / D = 40
-fn solve1(path: []const u8) !i32 {
+fn solve1(path: []const u8) !i64 {
+    return try solve(path, 0);
+}
+
+fn solve2(path: []const u8) !i64 {
+    return try solve(path, 10000000000000);
+}
+
+fn solve(path: []const u8, shift: i64) !i64 {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     const file = try fs.cwd().openFile(path, .{});
@@ -26,7 +34,7 @@ fn solve1(path: []const u8) !i32 {
     const input = try file.readToEndAlloc(allocator, math.maxInt(u32));
     defer allocator.free(input);
     var blocks = mem.tokenizeSequence(u8, input, "\n\n");
-    var total: i32 = 0;
+    var total: i64 = 0;
     while (blocks.next()) |block| {
         var lines = mem.tokenizeSequence(u8, block, "\n");
         const lineA = lines.next().?;
@@ -34,12 +42,14 @@ fn solve1(path: []const u8) !i32 {
         const lineB = lines.next().?;
         const coordsB = try parseCoords(lineB, "X+", "Y+");
         const linePrize = lines.next().?;
-        const coordsPrize = try parseCoords(linePrize, "X=", "Y=");
+        var coordsPrize = try parseCoords(linePrize, "X=", "Y=");
+        coordsPrize.x += shift;
+        coordsPrize.y += shift;
         const D = coordsA.x * coordsB.y - coordsA.y * coordsB.x;
         const Da = coordsPrize.x * coordsB.y - coordsPrize.y * coordsB.x;
         const Db = coordsA.x * coordsPrize.y - coordsA.y * coordsPrize.x;
-        const a = math.divExact(i32, Da, D) catch continue;
-        const b = math.divExact(i32, Db, D) catch continue;
+        const a = math.divExact(i64, Da, D) catch continue;
+        const b = math.divExact(i64, Db, D) catch continue;
         if (a >= 0 or b >= 0) {
             total += (a * 3 + b);
         }
@@ -47,18 +57,26 @@ fn solve1(path: []const u8) !i32 {
     return total;
 }
 
-fn parseCoords(line: []const u8, xPrefix: []const u8, yPrefix: []const u8) !struct { x: i32, y: i32 } {
+fn parseCoords(line: []const u8, xPrefix: []const u8, yPrefix: []const u8) !struct { x: i64, y: i64 } {
     const xStr = line[mem.indexOf(u8, line, xPrefix).? + xPrefix.len .. mem.indexOf(u8, line, ",").?];
     const yStr = line[mem.indexOf(u8, line, yPrefix).? + yPrefix.len ..];
-    const x = try fmt.parseInt(i32, xStr, 10);
-    const y = try fmt.parseInt(i32, yStr, 10);
+    const x = try fmt.parseInt(i64, xStr, 10);
+    const y = try fmt.parseInt(i64, yStr, 10);
     return .{ .x = x, .y = y };
 }
 
-test "input 0" {
+test "part 1 input 0" {
     try testing.expectEqual(480, try solve1("src/day13/in0.txt"));
 }
 
-test "input 1" {
+test "part 1 input 1" {
     try testing.expectEqual(40369, try solve1("src/day13/in1.txt"));
+}
+
+test "part 2 input 0" {
+    try testing.expectEqual(875318608908, try solve2("src/day13/in0.txt"));
+}
+
+test "part 2 input 1" {
+    try testing.expectEqual(72587986598368, try solve2("src/day13/in1.txt"));
 }
